5f908367bf0c63273cd732fca92c9a96cc54574b
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2020-01-31 14:58:09 +0530

    Nav-bar and cafe: WIP

--- CapCafeApp/src/app/app-routing.module.ts
+++ CapCafeApp/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CafeComponent } from './cafe/cafe.component';
+import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cafe',component: CafeComponent},
+  {path: 'helpdesk', component: HelpdeskComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'orders', component: OrdersComponent},
+  {path: 'reviews', component: ReviewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
