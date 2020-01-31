e82ae7aeade5b4e1223dc32be5f23befd3c75778
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2020-01-31 12:12:42 +0530

    Initialisation

--- CapCafeApp/src/app/app.module.ts
+++ CapCafeApp/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { CafeComponent } from './cafe/cafe.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CafeComponent,
+    MenuComponent,
+    OrdersComponent,
+    HelpdeskComponent,
+    ReviewsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
