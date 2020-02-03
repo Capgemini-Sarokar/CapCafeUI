e236ba8bbc93e54bfe4519f6a08fec4f4ccbeb0d
Kunal Roychoudhury <lanuks97@gmail.com>
2020-02-03 12:20:13 +0530

    finished Navbar

--- CapCafeApp/src/app/app.module.ts
+++ CapCafeApp/src/app/app.module.ts
@@ -4,23 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { CafeComponent } from './cafe/cafe.component';
-import { MenuComponent } from './menu/menu.component';
-import { OrdersComponent } from './orders/orders.component';
-import { HelpdeskComponent } from './helpdesk/helpdesk.component';
-import { ReviewsComponent } from './reviews/reviews.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    CafeComponent,
-    MenuComponent,
-    OrdersComponent,
-    HelpdeskComponent,
-    ReviewsComponent,
     NavbarComponent
   ],
   imports: [
