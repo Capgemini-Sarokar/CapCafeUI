2297b884773d4132612c2a57c9c5e8d9f01fce87
Kunal Roychoudhury <lanuks97@gmail.com>
2020-02-03 22:03:16 +0530

    added material login form

--- CapCafeApp/src/app/app-routing.module.ts
+++ CapCafeApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CafeComponent } from './cafe/cafe.component';
 import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 const routes: Routes = [
   { path:'cafe', component: CafeComponent },
-  { path:'helpdesk', component: HelpdeskComponent }
+  { path:'helpdesk', component: HelpdeskComponent },
+  { path:'login', component: LoginSignupComponent}
 ];
 
 @NgModule({
--- CapCafeApp/src/app/app.module.ts
+++ CapCafeApp/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CafeComponent } from './cafe/cafe.component';
 import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CafeComponent,
-    HelpdeskComponent
+    HelpdeskComponent,
+    LoginSignupComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
