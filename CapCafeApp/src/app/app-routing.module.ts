477ed0848e00d28707152dbd2acc4d942934f06c
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2020-02-04 15:28:03 +0530

    changes #2

--- CapCafeApp/src/app/app-routing.module.ts
+++ CapCafeApp/src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ import { AddCafeComponent } from './add-cafe/add-cafe.component';
 import { RemoveCafeComponent } from './remove-cafe/remove-cafe.component';
 import { UpdateCafeComponent } from './update-cafe/update-cafe.component';
 import { ViewCafeComponent } from './view-cafe/view-cafe.component';
+import { AddMenuComponent } from './add-menu/add-menu.component';
+import { ViewMenuComponent } from './view-menu/view-menu.component';
+import { RemoveMenuComponent } from './remove-menu/remove-menu.component';
+import { UpdateMenuComponent } from './update-menu/update-menu.component';
 
 const routes: Routes = [
   { path:'add-cafe', component: AddCafeComponent },
   { path: 'remove-cafe', component: RemoveCafeComponent},
   { path: 'update-cafe', component: UpdateCafeComponent},
   { path: 'view-cafe', component: ViewCafeComponent},
-  { path: 'helpdesk', component: HelpdeskComponent }
+  { path: 'helpdesk', component: HelpdeskComponent },
+  { path: 'add-menu', component: AddMenuComponent },
+  { path: 'remove-menu', component: RemoveMenuComponent },
+  { path: 'update-menu', component: UpdateMenuComponent},
+  { path: 'view-menu', component: ViewMenuComponent}
 ];
 
 @NgModule({
