bde1037e7a19acdccd4986302f84e665919fca77
im4gingchords <55138357+im4gingchords@users.noreply.github.com>
2020-02-10 12:15:59 +0530

    added ui

--- CapCafeApp/src/app/menu-order/menu-order-routing.module.ts
+++ CapCafeApp/src/app/menu-order/menu-order-routing.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuOrderComponent } from './menu-order.component';
-import { ViewMenuComponent } from './view-menu/view-menu.component';
-import { AddMenuComponent } from './add-menu/add-menu.component';
-import { UpdateMenuComponent } from './update-menu/update-menu.component';
-import { RemoveMenuComponent } from './remove-menu/remove-menu.component';
+import { MenuComponent} from './menu/menu.component';
+import {AddMenuComponent} from './menu/add-menu.component';
 
 const routes: Routes = [
   { path: '', component: MenuOrderComponent,
   children: [
-    { path: 'viewMenu', component: ViewMenuComponent },
-    { path: 'addMenu', component: AddMenuComponent },
-    { path: 'updateMenu', component: UpdateMenuComponent },
-    { path: 'removeMenu', component: RemoveMenuComponent }
+    {path: 'menus', component:MenuComponent},
+    {path:'add', component:AddMenuComponent}
   ]
   }
 ]
