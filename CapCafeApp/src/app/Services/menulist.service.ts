41214abca11cabba9d0cab49a1f567f9b8efc6b8
Kunal Roychoudhury <lanuks97@gmail.com>
2020-02-25 14:21:37 +0530

    Update menulist.service.ts

--- CapCafeApp/src/app/Services/menulist.service.ts
+++ CapCafeApp/src/app/Services/menulist.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Menu } from '../models/menu';
+import { Menu } from '../models/menu.model';
 import { Ordering } from '../models/ordering';
 
 const httpOptions ={
